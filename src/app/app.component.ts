6e33af2f8476ad185d88b31e86599f4b287e2446
FANE Yaya <yaya.fane@vinci-energies.net>
2023-02-14 15:00:47 +0100

    claire ajt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
 
-  ngOnInit(){}
+  ngOnInit(){
+    const interval$ = interval(1000);
+
+    interval$.subscribe(value => console.log(value));
+  }
  }                                                                                                                                                  
