1ecbcd28bea3a26e90637581a310b13f9bab61d9
FANE Yaya <yaya.fane@vinci-energies.net>
2023-02-28 14:16:16 +0100

    ajout dun dossier models qui inclus  tt

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
-import { FaceSnap } from '../models/face-snap.model'; 
-import { FaceSnapsService } from '../services/face-snaps.service';
+import { FaceSnap } from '../core/models/face-snap.model'; 
+import { FaceSnapsService } from '../core/services/face-snaps.service';
 import { Observable } from 'rxjs';
 
 @Component({
