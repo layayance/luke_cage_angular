2f151ef8afa259be26400a20a01a0cbaf6809884
yaya.fane <y.faneyaya@gmail.com>
2023-03-01 17:26:59 +0100

    authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { AuthModule } from './auth/auth.module'
 
 @NgModule({
   declarations: [
     AppComponent,
- 
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     CoreModule,
-    LandingPageModule
+    LandingPageModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
- 
+export class AppModule {
+
 }
